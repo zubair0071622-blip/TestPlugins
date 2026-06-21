b83a8ba6582db8f9e09dbdc8351ce72398e0e5e3
zubair0071622-blip <zubair0071622@gmail.com>
2026-06-21 09:55:01 +0500

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -4,7 +4,7 @@ import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.LoadResponse
-import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.SubtitleFile 
 import com.lagradost.cloudstream3.ExtractorLink
 
 class ExampleProvider : MainAPI() { 
